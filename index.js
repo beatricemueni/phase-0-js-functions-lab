08e58d16d47bf58565ff5315c36e376cb2fac9bc
beatricemueni <muenisophie8@gmail.com>
2026-04-10 12:38:24 +0300

    Modified index.js

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
+// Function 1: calculateTax
+function calculateTax(amount) {
+  return amount * 0.1;
+}
 
+// Function 2: convertToUpperCase
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
+
+// Function 3: findMaximum
+function findMaximum(num1, num2) {
+  return num1 > num2 ? num1 : num2;
+}
+
+// Function 4: isPalindrome (FIXED: case-insensitive)
+function isPalindrome(word) {
+  const cleaned = word.toLowerCase();
+  const reversed = cleaned.split('').reverse().join('');
+  return cleaned === reversed;
+}
+
+// Function 5: calculateDiscountedPrice
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  return originalPrice - (originalPrice * discountPercentage / 100);
+}
 
 
 
